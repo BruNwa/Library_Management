334aa81d3fa68e14823fabb5f348ccb998f07633
Zaim Anwar <106646716+BruNwa@users.noreply.github.com>
2024-10-16 20:59:52 +0200

    database access fix

--- libraryApp/startup.sh
+++ libraryApp/startup.sh
@@ -2,26 +2,26 @@
 
 while true; do
     clear
-    echo "                                         *******************************************************"
-    echo "                                         *               📚 LIBRARY MANAGEMENT 📚              *"
-    echo "                                         *******************************************************"
-    echo "                                         *                                                     *"
-    echo "                                         *                1. 📖 Book Management                *"
-    echo "                                         *                2. 👥 User Management                *"
-    echo "                                         *                3. 📅 Borrow/Return                  *"
-    echo "                                         *                4. 💸 Fines                          *"
-    echo "                                         *            m3lib. 🐳 Enter the app container        *"
-    echo "                                         *         database. 🗄️ Access the database containe   *"
-    echo "                                         *             exit. 🚪 Exit the m3lib app             *"
-    echo "                                         *                                                     *"
-    echo "                                         *******************************************************"
-    echo "                                         *                Please select an option :             *"
-    echo "                                         *******************************************************"
+    echo "*******************************************************"
+    echo "*                 📚 LIBRARY MANAGEMENT 📚            *"
+    echo "*******************************************************"
+    echo "*                                                     *"
+    echo "*                1. 📖 Book Management                *"
+    echo "*                2. 👥 User Management                *"
+    echo "*                3. 📅 Borrow/Return                  *"
+    echo "*                4. 💸 Fines                          *"
+    echo "*            m3lib. 🐳 Enter the app container        *"
+    echo "*         database. 🗄️ Access the database containe   *"
+    echo "*             exit. 🚪 Exit the m3lib app             *"
+    echo "*                                                     *"
+    echo "*******************************************************"
+    echo "*             Please select an option :               *"
+    echo "*******************************************************"
     read -p "Choose an option: " choice
 
     case $choice in
         1)  clear
-            ./book_management.sh ;;
+            ./book_management.sh ;; 
         2)  clear
             ./user_management.sh ;;
         3)  clear
@@ -31,7 +31,7 @@ while true; do
         "m3lib")clear
             bash;;
         "database") clear
-            mysql -D $DB_Name;;
+            mysql -D $DB_NAME;;
         "exit") clear
             exit;;
         *) echo "Invalid option. Please try again."
