ca1a1292cdc310bade6ab2fbe67d8589ae4a7105
Yurii <maisooradze@gmail.com>
2024-10-16 13:54:28 +0000

    Added options to cancel fine or all fines and to lookup by user

--- libraryApp/fine_management.sh
+++ libraryApp/fine_management.sh
@@ -24,20 +24,46 @@ mysql -D $DB_NAME -e \
     JOIN Books B ON L.book_id = B.book_id
     LIMIT 5;"
 }
+show_user_fines() {
+    read -p "Enter User ID to lookup fines: " user_id
+    echo "Fines for User ID $user_id:"
+    mysql -D $DB_NAME -e \
+        "SELECT O.fine_id, B.title, O.fine_amount, L.due_date
+        FROM Overdue_Fines O
+        JOIN Borrow_Log L ON O.borrow_id = L.borrow_id
+        JOIN Books B ON L.book_id = B.book_id
+        WHERE L.user_id = $user_id;"
+}
+cancel_fine() {
+    read -p "Enter Fine ID to cancel: " fine_id
+    mysql -D $DB_NAME -e \
+        "DELETE FROM Overdue_Fines WHERE fine_id = $fine_id;"
+    echo "Fine ID $fine_id has been canceled."
+}
+
+cancel_all_fines() {
+    mysql -D $DB_NAME -e "DELETE FROM Overdue_Fines;"
+    echo "All fines have been canceled."
+}
 
 while true; do
     echo " ============================== "
     echo "|      Fine Management Menu    |"
     echo " ============================== "
-    echo " | 1. Show fines              | "
-    echo " | 5. Exit                    | "
+    echo " | 1. Show fines                                                     | "
+    echo " | 2. Lookup fines by user                                           | "
+    echo " | 3. Cancel a single fine                                           | "
+    echo " | 4. Cancel all fines(is it some kind of a Christmas present?       | "
+    echo " | 5. Exit                                                           | "
     echo "  ----------------------------  "
     read -p "Choose an option: " choice
 
     case $choice in
         1) show_report ;;
+        2) show_user_fines ;;
+        3) cancel_fine ;;
+        4) cancel_all_fines ;;
         5) exit 0 ;;
         *) echo "Invalid option. Please try again." ;;
     esac
-
 done
